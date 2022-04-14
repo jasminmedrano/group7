9b4e0e194e945457cd0e0566e21c135123243cb3
jasminmedrano <78002503+jasminmedrano@users.noreply.github.com>
2022-04-13 22:15:06 -0700

    xcv

--- js/lab.js
+++ js/lab.js
@@ -13,3 +13,27 @@ $("input").click(function(){
 $("#jag-close").click(function(){
   $("#jaguar .info").toggleClass("hidden");
 })
+
+//navbar animation
+var navBar = $('.navbar');
+navBar.append('<li class="marker"></li>');
+
+navBar.on("click" , "a" , function(){
+
+    var navTap = $(this).closest('.navbar');
+    var mrkWidth = $(this).parent('li').width();
+    var marker = navTap.find('.marker');
+    var nx = $(".navbar").offset();
+    var lx = $(this).parent('li').offset();
+    var left = lx.left - nx.left;
+
+   navBar.find('li').removeClass('active');
+
+   $(this).parent().addClass('active');
+
+    marker.removeClass("anim").css({ "width" : mrkWidth , "left" : left });
+    setTimeout(function(){
+      marker.addClass("anim")
+    },200);
+
+});
